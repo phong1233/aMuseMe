59598efccc0413578ed2ecd68ee68bbd9a34449f
welleh <kevinjiang64@hotmail.com>
2020-01-25 22:56:21 -0500

    changed services and app

--- app.py
+++ app.py
@@ -9,11 +9,21 @@ services = Services()
 def home():
     return "Hello, hello!"
 
-@app.route("/pause", methods=['GET'])
+@app.route("/pause", methods=['POST'])
 def pause():
     return services.pause_song()
+@app.route("/play", methods=['POST'])
+def play():
+    return services.play_song()
+@app.route("/unpause", methods=['POST'])
+def unpause():
+    return services.unpause.song()
+@app.route("/stop", methods=['POST'])
+def stop():
+    return services.stop_song()
+@app.route("/status", methods=['GET'])
+def status():
+    return services.status_song()
 
-
-    
 if __name__ == "__main__":
     app.run(debug=True)
--- services.py
+++ services.py
@@ -8,3 +8,32 @@ class Services():
         text = response.read()
         return text.decode('ascii')
 
+    def play_song(self):
+        conn = http.client.HTTPConnection("localhost", 8080)
+        conn.request("POST", "/play")
+        response = conn.getresponse()
+        text = response.read()
+        return text.decode('ascii')
+
+    def unpause_song(self):
+        conn = http.client.HTTPConnection("localhost", 8080)
+        conn.request("POST", "/unpause")
+        response = conn.getresponse()
+        text = response.read()
+        return text.decode('ascii')
+
+    def stop_song(self):
+        conn = http.client.HTTPConnection("localhost", 8080)
+        conn.request("POST", "/stop")
+        response = conn.getresponse()
+        text = response.read()
+        return text.decode('ascii')
+
+    def status_song(self):
+        conn = http.client.HTTPConnection("localhost", 8080)
+        conn.request("GET", "/status")
+        response = conn.getresponse()
+        text = response.read()
+        return text.decode('ascii')
+
+
